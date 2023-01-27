e999ad559f8e85ddf04926443befd7095fd229de
Zelto Lira <you@example.com>
2023-01-27 05:06:03 -0300

    ajusta mapeamento Id UUID

--- src/main/java/br/com/petz/clientepet/pet/domain/Cliente.java
+++ src/main/java/br/com/petz/clientepet/pet/domain/Cliente.java
@@ -6,7 +6,10 @@ import java.util.UUID;
 
 import org.hibernate.validator.constraints.br.CPF;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -21,6 +24,8 @@ import lombok.NoArgsConstructor;
 public class Cliente {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
 	private UUID idCliente;
 	@NotBlank
 	private String nomeCompleto;
@@ -43,7 +48,6 @@ public class Cliente {
 	public Cliente(UUID idCliente, @NotBlank String nomeCompleto, @NotBlank @Email String email,
 			@NotBlank String celular, String telefone, Sexo sexo, @NotNull LocalDate dataNasciemento, @CPF String cpf,
 			Boolean aceitaTermos) {
-		this.idCliente = UUID.randomUUID();
 		this.idCliente = idCliente;
 		this.nomeCompleto = nomeCompleto;
 		this.email = email;
